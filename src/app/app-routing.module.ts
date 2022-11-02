2de9f82b0d2fe2c91327c28b8015e5141a3ad53c
nada.h <nada.h22425@gmail.com>
2022-11-02 11:39:01 +0200

    start

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  
+    {
+      path: 'account', 
+      loadChildren: () => import('./components/user/user.module').then(m => m.UserModule)
+    },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
